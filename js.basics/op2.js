9ebd259cd8e160a2d25dfb5c7d74dd239e62a4a4
tripathi4251 <tripathi.4251@gmail.com>
2026-06-27 18:17:18 +0530

    add operator example

---
+++ js.basics/op2.js
@@ -0,0 +1,23 @@
+/*
+assignment operator
+let x=10;
+
+console.log("x= " , x+=5);
+console.log("x=" , x-=5);
+console.log("x = " , x*=5);
+console.log("x=", x/5);
+console.log("x=",x**5);
+*/
+
+/*
+comparison operator
+let x=15;
+let y=10;
+
+console.log(x==y);
+console.log(x!=y);
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+*/
