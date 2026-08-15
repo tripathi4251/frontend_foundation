678bf3d6acac088df95b86be06373b26d9b30602
tripathi4251 <tripathi.4251@gmail.com>
2026-08-15 16:47:29 +0530

    add js array codes

--- js.basics/array1.js
+++ js.basics/array1.js
@@ -1,2 +1,24 @@
+/*
 let cars=["saab","volvo","bmw"];
 console.log(cars);
+console.log(cars.length);
+*/
+
+//for changing an array element
+/*
+let cars = [
+    "saab",
+    "volvo",
+    "bmw" ,
+]
+cars[0]="opel";
+console.log(cars);
+console.log(typeof cars);
+*/
+
+/*
+const fruits=["apple","mango","grapes","raspberry"]
+let mylist= fruits.toString();
+console.log(mylist);
+*/
+
---
+++ js.basics/array2.js
@@ -0,0 +1,6 @@
+// looping over an array
+
+let heroes =["ironman","thor","hulk","shaktiman","antman"];
+for (let hero of heroes) {
+    console.log(hero);
+}
