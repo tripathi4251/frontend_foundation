83769fdb8d3fc24f33d40ff6fd294ee6457ff099
tripathi4251 <tripathi.4251@gmail.com>
2026-08-15 17:31:53 +0530

    add array codes

---
+++ js.basics/array3.js
@@ -0,0 +1,22 @@
+// array methods
+//to string
+const fruits=["banana", "orange", "apple" , "mango"];
+let mylist= fruits.toString();
+console.log (mylist);
+
+//create an array to store comapanies ["bloomberg","uber","microsoft","google","ibm","netflix"]
+// remove the first company from the array
+
+
+let companies=["bloomberg","microsoft","uber","google","IBM","netflix"]
+companies.shift();
+
+
+let companies1=["bloomberg","microsoft","uber","google","IBM","netflix"];
+companies1.splice(2,1,"ola");
+
+
+let companies2=["bloomberg","microsoft","uber","google","IBM","netflix"];
+companies2.push("amazon");
+
+
