3fcf4a64bce719a2b3c3d8949b237ffebaaf68fe
tripathi4251 <tripathi.4251@gmail.com>
2026-08-16 00:41:17 +0530

    add js functions

--- js.basics/function2.js
+++ js.basics/function2.js
@@ -3,4 +3,19 @@ function substraction(x,y) {
     console.log(x-y);
 }
 
+// example 2
+function sum(x,y) {
+    s=x+y;
+    return s;
+}
 
+let val=sum(3,4);
+console.log(val);
+
+
+// example 3
+function multiply(x,y) {
+    return x*y;
+}
+
+console.log(multiply(2,3));
