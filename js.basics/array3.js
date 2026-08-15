207dbdd0eddd890406ca023ebced991048f230ea
tripathi4251 <tripathi.4251@gmail.com>
2026-08-15 18:00:09 +0530

    add this array

--- js.basics/array3.js
+++ js.basics/array3.js
@@ -7,16 +7,21 @@ console.log (mylist);
 //create an array to store comapanies ["bloomberg","uber","microsoft","google","ibm","netflix"]
 // remove the first company from the array
 
-
+/*
 let companies=["bloomberg","microsoft","uber","google","IBM","netflix"]
 companies.shift();
+*/
 
-
+/*
 let companies1=["bloomberg","microsoft","uber","google","IBM","netflix"];
 companies1.splice(2,1,"ola");
+*/
 
 
+/*
 let companies2=["bloomberg","microsoft","uber","google","IBM","netflix"];
 companies2.push("amazon");
+*/
+
 
 
---
+++ js.basics/array4.js
@@ -0,0 +1,20 @@
+let fruit=["banana","orange","apple","mango"];
+fruit.pop();
+console.log(fruit);
+
+let car=["suzuki","maruti","alto"]
+car.push("ferrari");
+console.log(car);
+
+let vegetables=["potato","tomato","onion"];
+vegetables.shift();
+console.log(vegetables);
+
+let subjects=["amaths","science","physics","commerce"];
+subjects.unshift("hindi");
+console.log(subjects);
+
+let boys=["akshat","kartik","ayush","jyotesh"];
+let girls=["ayushi","diksha","agnija"];
+let mychildren=girls.concat(boys);
+console.log(mychildren);
