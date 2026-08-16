74e25358c579d75a7bb8fbf3c16e96384d7b7ec0
tripathi4251 <tripathi.4251@gmail.com>
2026-08-16 14:15:20 +0530

    add js functions

--- js.basics/function3.js
+++ js.basics/function3.js
@@ -6,3 +6,11 @@ let evenarr =arr1.filter((val) => {
 });
 
 console.log(evenarr);
+
+//we are given array of marks of students. filter out of the marks of the student that scores 90%..
+let marks = [97,64,32,49,99,96,86];
+let toppers=marks.filter((val) => {
+    return val>90;
+});
+
+console.log(toppers);
