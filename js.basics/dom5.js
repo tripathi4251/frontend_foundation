5e2742a7ceb345e2f8b47e923bb28b3fafec7643
tripathi4251 <tripathi.4251@gmail.com>
2026-09-03 18:49:06 +0530

    add dom js

--- js.basics/dom5.js
+++ js.basics/dom5.js
@@ -1,3 +1,4 @@
+//que 1
 let newBtn=document.createElement("button");
 newBtn.innerText="click me";
 console.log(newBtn);
@@ -7,3 +8,6 @@ newBtn.style.backgroundColor ="red";
 
 let div=document.querySelector("body");
 div.prepend(newBtn);
+
+//que 2
+let para =document.querySelector("p");
