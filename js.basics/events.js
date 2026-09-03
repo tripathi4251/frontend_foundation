b86b5d7588f85e358a28b353eab1039eba74cfa3
tripathi4251 <tripathi.4251@gmail.com>
2026-09-04 02:45:29 +0530

    add js events

---
+++ js.basics/events.js
@@ -0,0 +1,9 @@
+//let button=document.querySelector("#btn");
+//button.addEventListener("click",function() {
+ //     button.innerText="hello javascript";
+//});
+
+let button=document.querySelector("#btn");
+button.addEventListener("click",function() {
+    console.log("button was clicked");
+});
